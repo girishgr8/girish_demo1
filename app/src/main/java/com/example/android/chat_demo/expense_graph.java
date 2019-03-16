3239c2b374a34c50d6f4c191c1a98766c40cd1b8
Girish <giryagr8@gmail.com>
2019-03-16 16:40:29 +0530

    UI PieChart

--- app/src/main/java/com/example/android/chat_demo/expense_graph.java
+++ app/src/main/java/com/example/android/chat_demo/expense_graph.java
@@ -1,11 +1,19 @@
 package com.example.android.chat_demo;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
@@ -13,8 +21,11 @@ import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.ViewPortHandler;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -22,41 +33,57 @@ import java.util.ArrayList;
 
 public class expense_graph extends AppCompatActivity {
     private static String TAG = "Main Activity";
-    private float[] expenses = {23.2f,45.1f,15.32f,19.78f,58.65f,3.45f};
-    private String[] monthName = {"January","February","March","April","May","June"};
+    private int[] expenses = {350,500,960,830,220,300,560,700/*,415,230*/};
+    private String[] category = {"Food","Electronics","Transport","Gift","Bills","Home","Miscellaneous","Balance"/*,"Education","Child Care"*/};
     PieChart pieChart;
+    private RelativeLayout relativeLayout;
+    Legend legend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_graph);
         Log.d(TAG, "onCreate: starting to create pie chart");
+        pieChart = new PieChart(this);
+        relativeLayout = (RelativeLayout) findViewById(R.id.layout_id);
+        relativeLayout.addView(pieChart);
+        relativeLayout.setBackgroundColor(Color.WHITE);
         pieChart = (PieChart) findViewById(R.id.idPieChart);
         Description description = new Description();
-        description.setText("Monthly expenses");
+        pieChart.setUsePercentValues(true);
+        pieChart.setRotationEnabled(true);
+        pieChart.setHoleRadius(50f);
+        description.setPosition(5f,5f);
+        description.setTextSize(13f);
         pieChart.setDescription(description);
+        pieChart.setDrawHoleEnabled(true);
+        pieChart.setCenterText("Expenses Per Category");
+        pieChart.setCenterTextSize(18f);
+        pieChart.setHighlightPerTapEnabled(true);
+        pieChart.setTransparentCircleAlpha(0);
         pieChart.setDrawEntryLabels(true);
+        pieChart.setEntryLabelColor(Color.BLACK);
+        pieChart.setEntryLabelTextSize(13f);
+        pieChart.animateXY(1500,1500);
         addDataSet();
         pieChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
             @Override
             public void onValueSelected(Entry e, Highlight h) {
+                if(e==null)
+                    return;
 
-                int pos = e.toString().indexOf("(sum): ");
-                String exp = e.toString().substring(pos+6);
-
-                for (int i=0;i<expenses.length;i++){
-                    if (expenses[i] == Float.parseFloat(exp)){
-                        pos = i;
-                        break;
-                    }
-                }
-                String mon = monthName[pos+1];
-                Toast.makeText(expense_graph.this, "Month: "+mon +"\n"+"Expense: "+exp,Toast.LENGTH_LONG).show();
+                int ind = findIndex(e);
+                if(ind!=category.length-1)
+                    customDialog("Expense","Category: "+category[ind]+"\nExpense: $"+e.getY(),"Cancel","Ok");
+                    //Toast.makeText(expense_graph.this,"Category: "+category[ind]+" is: "+e.getY(),Toast.LENGTH_SHORT).show();
+                else
+                    customDialog("Expense","Balance Left: $"+e.getY(),"Cancel","Ok");
+                    //Toast.makeText(expense_graph.this,"Balance Left: "+e.getY(),Toast.LENGTH_SHORT).show();
             }
-
+            /*"Month: "+ monthName[h.getDataIndex()] +"\n"+"Expense: "+expenses[h.getDataIndex()]*/
             @Override
             public void onNothingSelected() {
-
+                Toast.makeText(expense_graph.this,"Nothing selected!!",Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -66,34 +93,69 @@ public class expense_graph extends AppCompatActivity {
         ArrayList<PieEntry> expense_entry = new ArrayList<>();
         ArrayList<String> months_entry = new ArrayList<>();
         for (int i=0;i<expenses.length;i++){
-            expense_entry.add(new PieEntry(expenses[i],i));
+            expense_entry.add(new PieEntry(expenses[i],category[i]));
         }
-        for (int i=0;i<monthName.length;i++){
-            months_entry.add(monthName[i]);
+        for (int i=0;i<category.length;i++){
+            months_entry.add(category[i]);
         }
 
         PieDataSet pieDataSet = new PieDataSet(expense_entry,"Expenses");
-        pieDataSet.setSliceSpace(2);
-        pieDataSet.setValueTextSize(12);
+        pieDataSet.setSliceSpace(3f);
+        pieDataSet.setValueTextSize(10f);
+        /*pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        pieDataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);*/
+        //ArrayList<Integer> color_names = new ArrayList<>();
+        /*color_names.add(Color.rgb(255,247,0));
+        color_names.add(Color.rgb(255,131,0));
+        color_names.add(Color.rgb(253,255,129));
+        color_names.add(Color.rgb(255,188,12));
+        color_names.add(Color.rgb(255,208,0));
+        color_names.add(Color.rgb(241,196,15));
+        color_names.add(Color.rgb(255,108,0));
+        color_names.add(Color.rgb(78,255,0));*/
+        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
 
-        ArrayList<Integer> color_names = new ArrayList<>();
-        color_names.add(Color.GRAY);
-        //color_names.add(Color.BLUE);
-        color_names.add(Color.RED);
-        color_names.add(Color.GREEN);
-        color_names.add(Color.CYAN);
-        color_names.add(Color.YELLOW);
-        color_names.add(Color.MAGENTA);
-        pieDataSet.setColors(color_names);
-        Legend legend = pieChart.getLegend();
-        legend.setForm(Legend.LegendForm.CIRCLE);
-        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
-        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        legend = pieChart.getLegend();
+
+        legend.setForm(Legend.LegendForm.SQUARE);
+        legend.setOrientation(Legend.LegendOrientation.VERTICAL);
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setFormSize(10f);
+        legend.setWordWrapEnabled(true);
+        legend.setYEntrySpace(10f);
+        legend.setFormToTextSpace(10f);
         legend.setDrawInside(false);
-
         PieData pieData = new PieData(pieDataSet);
         pieChart.setData(pieData);
-        pieChart.invalidate();
+        pieData.setValueFormatter(new PercentFormatter());
+        pieData.setValueTextSize(15f);
+        pieData.setValueTextColor(Color.BLACK);
+        pieDataSet.setSelectionShift(7f);
+    }
+
+    public int findIndex(Entry e){
+        int index = 0;
+        for(int i=0;i<expenses.length;i++){
+            if(expenses[i]==e.getY()){
+                index = i;
+                break;
+            }
+            else
+                continue;
+        }
+        return index;
+    }
+
+    public void customDialog(String title,String msg,final String cancel,final String ok){
+        final AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle(title);
+        alertDialogBuilder.setMessage(msg);
+        alertDialogBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // write toast if necessary...
+            }
+        });
+        alertDialogBuilder.show();
     }
 }
