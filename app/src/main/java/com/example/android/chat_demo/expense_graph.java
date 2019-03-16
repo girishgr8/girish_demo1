55d8ec5a479d4d9d70aa2738b99cd234f3ac2d11
Girish <giryagr8@gmail.com>
2019-03-16 23:49:59 +0530

    Pie Chart Final

--- app/src/main/java/com/example/android/chat_demo/expense_graph.java
+++ app/src/main/java/com/example/android/chat_demo/expense_graph.java
@@ -1,19 +1,12 @@
 package com.example.android.chat_demo;
 
-import android.app.Dialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.Toast;
-
-import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.Legend;
@@ -24,11 +17,6 @@ import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
-import com.github.mikephil.charting.utils.ColorTemplate;
-import com.github.mikephil.charting.utils.ViewPortHandler;
-import com.jjoe64.graphview.series.DataPoint;
-import com.jjoe64.graphview.series.LineGraphSeries;
-
 import java.util.ArrayList;
 
 public class expense_graph extends AppCompatActivity {
@@ -36,7 +24,6 @@ public class expense_graph extends AppCompatActivity {
     private int[] expenses = {350,500,960,830,220,300,560,700/*,415,230*/};
     private String[] category = {"Food","Electronics","Transport","Gift","Bills","Home","Miscellaneous","Balance"/*,"Education","Child Care"*/};
     PieChart pieChart;
-    private RelativeLayout relativeLayout;
     Legend legend;
 
     @Override
@@ -44,14 +31,15 @@ public class expense_graph extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_graph);
         Log.d(TAG, "onCreate: starting to create pie chart");
-        pieChart = new PieChart(this);
-        relativeLayout = (RelativeLayout) findViewById(R.id.layout_id);
+        /*relativeLayout = (RelativeLayout) findViewById(R.id.layout_id);
         relativeLayout.addView(pieChart);
-        relativeLayout.setBackgroundColor(Color.WHITE);
+        relativeLayout.setBackgroundColor(Color.WHITE);*/
         pieChart = (PieChart) findViewById(R.id.idPieChart);
         Description description = new Description();
         pieChart.setUsePercentValues(true);
         pieChart.setRotationEnabled(true);
+        /*pieChart.setExtraLeftOffset(5f);
+        pieChart.setExtraRightOffset(5f);*/
         pieChart.setHoleRadius(50f);
         description.setPosition(5f,5f);
         description.setTextSize(13f);
@@ -102,29 +90,32 @@ public class expense_graph extends AppCompatActivity {
         PieDataSet pieDataSet = new PieDataSet(expense_entry,"Expenses");
         pieDataSet.setSliceSpace(3f);
         pieDataSet.setValueTextSize(10f);
-        /*pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
-        pieDataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);*/
-        //ArrayList<Integer> color_names = new ArrayList<>();
-        /*color_names.add(Color.rgb(255,247,0));
+        //pieDataSet.setYValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        //pieDataSet.setXValuePosition(PieDataSet.ValuePosition.OUTSIDE_SLICE);
+        ArrayList<Integer> color_names = new ArrayList<>();
+        color_names.add(Color.rgb(255,247,0));
         color_names.add(Color.rgb(255,131,0));
         color_names.add(Color.rgb(253,255,129));
         color_names.add(Color.rgb(255,188,12));
         color_names.add(Color.rgb(255,208,0));
         color_names.add(Color.rgb(241,196,15));
         color_names.add(Color.rgb(255,108,0));
-        color_names.add(Color.rgb(78,255,0));*/
-        pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
-
-        legend = pieChart.getLegend();
-
-        legend.setForm(Legend.LegendForm.SQUARE);
+        color_names.add(Color.rgb(78,255,0));
+        pieDataSet.setColors(color_names);
+        pieChart.getLegend().setEnabled(false);
+        //pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+       /* legend = pieChart.getLegend();
+        legend.setXEntrySpace(5f);
+        legend.setXOffset(80f);
+        legend.setYOffset(150f);
+        legend.setForm(Legend.LegendForm.CIRCLE);
         legend.setOrientation(Legend.LegendOrientation.VERTICAL);
         legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
-        legend.setFormSize(10f);
+        legend.setFormSize(15f);
         legend.setWordWrapEnabled(true);
-        legend.setYEntrySpace(10f);
-        legend.setFormToTextSpace(10f);
+        legend.setFormToTextSpace(3f);
         legend.setDrawInside(false);
+        legend.setMaxSizePercent(0.95f);*/
         PieData pieData = new PieData(pieDataSet);
         pieChart.setData(pieData);
         pieData.setValueFormatter(new PercentFormatter());
@@ -158,4 +149,7 @@ public class expense_graph extends AppCompatActivity {
         });
         alertDialogBuilder.show();
     }
+
 }
+
+
